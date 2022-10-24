7c519d49bbc4d2f94194d06ca6c17d0ac9611f22
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-23 19:22:51 -0500

    Paso de datos a la vista a traves del controlador usando Model.addatribute

--- src/main/java/com/bolsadeides/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsadeides/springboot/web/app/controller/IndexController.java
@@ -1,14 +1,17 @@
 package com.bolsadeides.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/app/")
 public class IndexController {
 	
-	@RequestMapping	(value = {"/index", "/", "/home"}, method = RequestMethod.GET)
-	public String index(){
+	@RequestMapping	(value = {"/index", "/", "", "/home"}, method = RequestMethod.GET)
+	public String index(Model model){
+		model.addAttribute("titulo", "Aprendiendo Spring");
 		return "index";
 	}
 }
