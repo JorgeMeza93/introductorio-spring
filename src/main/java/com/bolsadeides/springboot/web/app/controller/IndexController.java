d1133b0fcceb84fb91eb9a08409286c7a54350b9
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-23 22:01:44 -0500

    utilizacion de directiva de thymeleaf en la vista de perfil

--- src/main/java/com/bolsadeides/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsadeides/springboot/web/app/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.bolsadeides.springboot.web.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bolsadeides.springboot.web.app.model.Usuario;
 
 @Controller
 @RequestMapping("/app/")
@@ -14,4 +19,24 @@ public class IndexController {
 		model.addAttribute("titulo", "Aprendiendo Spring");
 		return "index";
 	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model){
+		Usuario usuario1 = new Usuario();
+		usuario1.setNombre("Jorge");
+		usuario1.setApellido("Meza");
+		usuario1.setEmail("jorge@gmail.com");
+		model.addAttribute("usuario", usuario1);
+		model.addAttribute("titulo", "Perfil del usuario".concat(usuario1.getNombre()));
+		return "perfil";
+	}
+	@RequestMapping("/listar")
+	public String listar(Model model){
+		List<Usuario> usuarios = new ArrayList<>();
+		model.addAttribute("usuarios", usuarios);
+		model.addAttribute("titulo", "Listado de Usuarios");
+		return "listar";
+	}
+	
+	
 }
